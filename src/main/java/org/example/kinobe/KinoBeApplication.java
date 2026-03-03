08ae4aebd9300e7c3118736f11273946364abf72
YacqubJAD <yadi0001@stud.kea.dk>
2026-03-03 13:45:38 +0100

    pakke struktur

---
+++ src/main/java/org/example/kinobe/KinoBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.kinobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinoBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/kinobe/KinoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kinobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
