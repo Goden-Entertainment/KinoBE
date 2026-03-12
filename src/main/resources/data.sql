190f203c32c5f68837bd6215829ac2fc0f131c2c
YacqubJAD <yadi0001@stud.kea.dk>
2026-03-12 15:17:14 +0100

    Admin login

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,7 @@ SELECT 'Theater 1', 400 WHERE NOT EXISTS (SELECT 1 FROM theater WHERE name = 'Th
 
 INSERT INTO theater (name, capacity)
 SELECT 'Theater 2', 240 WHERE NOT EXISTS (SELECT 1 FROM theater WHERE name = 'Theater 2');
+
+INSERT INTO users(user_id, date, email, password, phonenumber, username)
+SELECT 1,'2002-11-6', 'TheGoat@gmail.com', 'Admin',
+       12345678, 'August'
