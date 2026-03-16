487fbb570572bfd535d50cbc275636d997e8d1d5
YacqubJAD <yadi0001@stud.kea.dk>
2026-03-16 04:45:29 +0100

    docker deployment

--- src/main/java/org/example/kinobe/controller/UserRestController.java
+++ src/main/java/org/example/kinobe/controller/UserRestController.java
@@ -24,7 +24,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@RequestMapping("/users")
+@CrossOrigin("*")
 @RestController
 public class UserRestController {
 
--- src/main/java/org/example/kinobe/model/Showing.java
+++ src/main/java/org/example/kinobe/model/Showing.java
@@ -24,6 +24,54 @@ public class Showing {
     @Enumerated(EnumType.STRING)
     private Status status;
 
+    public Integer getShowingId() {
+        return showingId;
+    }
+
+    public void setShowingId(Integer showingId) {
+        this.showingId = showingId;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public Theater getTheater() {
+        return theater;
+    }
+
+    public void setTheater(Theater theater) {
+        this.theater = theater;
+    }
+
     @ManyToOne
     @JoinColumn(name = "movieFK")
     private Movie movie;
@@ -31,4 +79,6 @@ public class Showing {
     @ManyToOne
     @JoinColumn(name = "theaterFK")
     private Theater theater;
+
+
 }
